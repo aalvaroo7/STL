d4fb157ddd0f9a3febb6e974d6c620ac3b5a01a3
aalvaroo7 <almarrom@myuax.com>
2023-11-14 19:39:13 +0100

    comit ruben web

---
+++ ej2_circulo.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+namespace Geometria {
+    double calcularAreaTriangulo(double base, double altura) {
+        return (base * altura) / 2;
+    }
+}
---
+++ ej2_main.cpp
@@ -0,0 +1,6 @@
+#include <iostream
+int main() {
+    std::cout << "Area del triangulo: " << Geometria::calcularAreaTriangulo(5, 3) << std::endl;
+    std::cout << "Area del circulo: " << Geometria::calcularAreaCirculo(3) << std::endl;
+    return 0;
+}

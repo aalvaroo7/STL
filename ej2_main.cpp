99e76ad153087c5fb224b27fadbc23704859d256
aalvaroo7 <almarrom@myuax.com>
2023-11-14 19:40:20 +0100

    comit yo+github

--- ej2_main.cpp
+++ ej2_main.cpp
@@ -1,4 +1,10 @@
-#include <iostream
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+#include "ej2_triangulo.cpp"
+#include "ej2_circulo.cpp"
+
 int main() {
     std::cout << "Area del triangulo: " << Geometria::calcularAreaTriangulo(5, 3) << std::endl;
     std::cout << "Area del circulo: " << Geometria::calcularAreaCirculo(3) << std::endl;

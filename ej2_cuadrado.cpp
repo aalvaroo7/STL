c88d185b53a3c7437f41bb30bb5562fd0fae3dc7
aalvaroo7 <almarrom@myuax.com>
2023-11-14 19:45:57 +0100

    comit yo+github

--- ej2_cuadrado.cpp
+++ ej2_cuadrado.cpp
@@ -1,1 +1,9 @@
 #include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+namespace Geometria {
+    double calcularAreaCuadrado(double base, double altura) {
+        return (base * altura);
+    }
+}
--- ej2_main.cpp
+++ ej2_main.cpp
@@ -5,10 +5,11 @@ using namespace std;
 #include "ej2_triangulo.cpp"
 #include "ej2_circulo.cpp"
 #include "ej2_rectangulo.cpp"
-
+#include "ej2_cuadrado.cpp"
 int main() {
     std::cout << "Area del triangulo: " << Geometria::calcularAreaTriangulo(5, 3) << std::endl;
     std::cout << "Area del circulo: " << Geometria::calcularAreaCirculo(3) << std::endl;
     std::cout << "Area del rectangulo: " << Geometria::calcularAreaRectangulo(5, 3) << std::endl;
+    cout << "Area del cuadrado: " << Geometria::calcularAreaCuadrado(5, 3) << std::endl;
     return 0;
 }

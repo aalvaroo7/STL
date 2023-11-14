4a21b92252953be43ed00df27b49891d0e3037d1
aalvaroo7 <almarrom@myuax.com>
2023-11-14 16:52:35 +0100

    comit yo+guithub

--- ej1_espacio_nombres.cpp
+++ ej1_espacio_nombres.cpp
@@ -8,29 +8,34 @@ namespace Matematicas {
     int resta(int a, int b) {
         return a - b;
     }
+
 //respuesta proporcionada por ruben a lo que le añado como nos ha sido requerido cuatro ejemplos
 //de espacio de nombres que seran
 //multiplicacion, division, potencia y raiz cuadrada
-        int multiplicacion(int a, int b) {
-            return a * b;
-        }
-            int division(int a, int b) {
-                return a / b;
+    int multiplicacion(int a, int b) {
+        return a * b;
+    }
 
-            }
-            int potencia(int a, int b) {
-                return a ^ b;
-            }
-            int raiz_cuadrada(int a, int b) {
-                return a ^ 1 / b;
-            }
+    int division(int a, int b) {
+        return a / b;
 
-            int main() {
-                cout << "Suma: " << Matematicas::suma(5, 3) << std::endl;
-                cout << "Resta: " << Matematicas::resta(5, 3) << std::endl;
-                cout << Matematicas::multiplicacion(5, 3) << std::endl;
-                cout << Matematicas::division(5, 3) << std::endl;
-                cout << Matematicas::potencia(5, 3) << std::endl;
-                cout << Matematicas::raiz_cuadrada(5, 3) << std::endl;
-                return 0;
-            }
+    }
+
+    int potencia(int a, int b) {
+        return a ^ b;
+    }
+
+    int raiz_cuadrada(int a, int b) {
+        return a ^ 1 / b;
+    }
+
+    int main() {
+        cout << "Suma: " << Matematicas::suma(5, 3) << std::endl;
+        cout << "Resta: " << Matematicas::resta(5, 3) << std::endl;
+        cout << Matematicas::multiplicacion(5, 3) << std::endl;
+        cout << Matematicas::division(5, 3) << std::endl;
+        cout << Matematicas::potencia(5, 3) << std::endl;
+        cout << Matematicas::raiz_cuadrada(5, 3) << std::endl;
+        return 0;
+    }
+}

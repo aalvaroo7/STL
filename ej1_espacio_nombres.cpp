a7b932ed4234e673aaf3ac2333475ec0586ca101
aalvaroo7 <almarrom@myuax.com>
2023-11-14 16:51:31 +0100

    comit yo+guithub

--- ej1_espacio_nombres.cpp
+++ ej1_espacio_nombres.cpp
@@ -4,28 +4,29 @@ namespace Matematicas {
     int suma(int a, int b) {
         return a + b;
     }
+
     int resta(int a, int b) {
         return a - b;
     }
 //respuesta proporcionada por ruben a lo que le añado como nos ha sido requerido cuatro ejemplos
 //de espacio de nombres que seran
 //multiplicacion, division, potencia y raiz cuadrada
+        int multiplicacion(int a, int b) {
+            return a * b;
+        }
+            int division(int a, int b) {
+                return a / b;
 
-namespace matematicas {
-    int multiplicacion(int a, int b) {
-        return a * b;
-    }
-namespace matematicas{
-    int division(int a, int b) {
-        return a / b;
-    }
-    int main() {
-        cout << "Suma: " << Matematicas::suma(5, 3) << std::endl;
-        cout << "Resta: " << Matematicas::resta(5, 3) << std::endl;
-        cout << multiplicacion::multiplicacion(5, 3) << std::endl;
-        cout << division::division(5, 3) << std::endl;
-        return 0;
-    }
+            }
+            int potencia(int a, int b) {
+                return a ^ b;
+            }
 
-}
 
+            int main() {
+                cout << "Suma: " << Matematicas::suma(5, 3) << std::endl;
+                cout << "Resta: " << Matematicas::resta(5, 3) << std::endl;
+                cout << Matematicas::multiplicacion(5, 3) << std::endl;
+                cout << Matematicas::division(5, 3) << std::endl;
+                return 0;
+            }
